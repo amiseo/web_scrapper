335b7f1195145d112b84caeb5d0fb6b9f74b982b
Andrés Mise Olivera <j4m0@outlook.com>
2019-04-28 08:28:57 -0500

    Se realizan consultas a los articulos, con las URL obtenidas

--- news_page_objects.py
+++ news_page_objects.py
@@ -4,7 +4,7 @@ import requests
 from common import config
 
 
-class HomePage:
+class NewsPage:
 
     def __init__(self, news_site_uid, url):
         self._config = config()['news_sites'][news_site_uid]
@@ -14,15 +14,6 @@ class HomePage:
 
         self._visit()
 
-    @property
-    def article_links(self):
-        link_list = []
-        for link in self._select(self._queries['homepage_article_links']):
-            if link and link.has_attr('href'):
-                link_list.append(link)
-
-        return set(self._url + link['href'] for link in link_list)
-
     def _select(self, query_string):
         return self._html.select(query_string)
 
@@ -30,3 +21,33 @@ class HomePage:
         response = requests.get(self._url)
         response.raise_for_status()
         self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+
+
+class HomePage(NewsPage):
+
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
+
+    @property
+    def article_links(self):
+        link_list = []
+        for link in self._select(self._queries['homepage_article_links']):
+            if link and link.has_attr('href'):
+                link_list.append(link)
+
+        return set(link['href'] for link in link_list)
+
+class ArticlePage(NewsPage):
+
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
+
+    @property
+    def body(self):
+        result = self._select(self._queries['article_body'])
+        return result[0].text if len(result) else ''
+
+    @property
+    def title(self):
+        result = self._select(self._queries['article_title'])
+        return result[0].text if len(result) else ''
